3a6ff04fab36c0f5d9f3c9c16b12dc27471b6331
unknown <Administrator@N203_P20_Tiny.M2Schule.local>
2026-05-21 15:23:48 +0200

    achterbahn-db simulator teil 5241

--- train.gd
+++ train.gd
@@ -3,20 +3,19 @@ extends CharacterBody3D
 var gas: float = 0
 @export var speed := 1
 @onready var Path = $".."
-@onready var clabel = $"Path3D/PathFollow3D/Train/Führerstand/speed_dsp"
+@onready var clabel = $"./Führerstand/speed_dsp"
 func _physics_process(delta):
 
 	# Direkt Tasten abfragen ohne Input Map
 	if Input.is_key_pressed(KEY_UP):
-		gas += 0.01
+		gas += 0.05
 	if Input.is_key_pressed(KEY_DOWN):
-		gas -= 0.015
+		gas -= 0.07
 	if Input.is_key_pressed(KEY_SPACE):
 		gas = 0
 	Path.progress -= gas * 0.01
 	
-	if clabel:
-		clabel.set_text(str(gas) + " KM/H")
+	clabel.set_text(str(floor(gas)) + " KM/H")
 	# Bewegung nach vorne (lokaler Z-Achse)
 	#velocity = transform.basis.z * gas * speed
 	#move_and_slide()
